46a8a98ef2cecd576a1ada2be3e9891f3ae0cafa
sargeras197 <serge.evdokimov2015@gmail.com>
2025-09-16 17:29:57 +0300

    1ві
    11і1

---
+++ bin.py
@@ -0,0 +1,19 @@
+import angr
+
+# Шлях до вашого бінарника
+binary_path = "binaries/main_ETS.exe"
+
+# Створюємо angr-проєкт
+project = angr.Project(binary_path, auto_load_libs=True)
+
+# Виводимо базову інформацію
+print("Binary:", project.loader.main_object.binary)
+print("Arch:", project.arch.name)
+print("Entry point:", hex(project.entry))
+
+# Побудова CFG (Control Flow Graph)
+cfg = project.analyses.CFGFast(normalize=True)
+
+# Вивід знайдених функцій
+for func in cfg.kb.functions.values():
+    print(f"Function: {func.name} at {hex(func.addr)}")
